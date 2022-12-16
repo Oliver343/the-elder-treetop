420232d5d55f5fdf595b27a18e5cb80d6e6fc38a
Oliver Sanderson <oliver_343@hotmail.com>
2022-12-17 00:07:45 +0100

    Added data tables

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { BodyComponent } from './body/body.component';
 import { PassVariableComponent } from './body/pass-variable/pass-variable.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { QuickComponent } from './admin/quick/quick.component';
+import { TableComponent } from './table/table.component';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
 @NgModule({
   declarations: [
@@ -14,8 +16,9 @@ import { QuickComponent } from './admin/quick/quick.component';
     BodyComponent,
     PassVariableComponent,
     QuickComponent,
+    TableComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule, NgxDatatableModule],
   providers: [],
   bootstrap: [AppComponent],
 })
